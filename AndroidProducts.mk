187787816153f6d6df86761a04bfbafb788972b5
LMO GerritBot <gerrit@libremobileos.com>
2024-04-19 19:39:42 +0000

    Adapt to LMODroid by device-add script

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_vela.mk
+    $(LOCAL_DIR)/lmodroid_vela.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_vela-user \
-    lineage_vela-userdebug \
-    lineage_vela-eng
+    lmodroid_vela-user \
+    lmodroid_vela-userdebug \
+    lmodroid_vela-eng
